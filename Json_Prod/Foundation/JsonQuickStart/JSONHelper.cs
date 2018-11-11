b095131d9285228f33289da8c53885938082e5a4
SHADOWFAX\chris <chris.williams@readwatchcreate.com>
2018-11-11 12:32:40 -0500

    Initial release

---
+++ Json_Prod/Foundation/JsonQuickStart/JSONHelper.cs
@@ -0,0 +1,59 @@
+﻿/********************************************************************************
+ * JSON QuickStart Library - General Elements used to manipulate JSON data 
+ * 
+ * NOTE: Adapted from Clinch.NET.JSON
+ * LICENSE: Free to use provided details on fixes and/or extensions emailed to 
+ *      chris.williams@readwatchcreate.com
+********************************************************************************/
+
+namespace JsonQuickStart
+{
+    using System.Xml;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Helps serialze to JSON and Deserialize to XML
+    /// V1.0.0.1 - Namspace and class name changed to proper case
+    /// </summary>
+    public class JsonHelper
+    {
+        /// <summary>
+        /// Converts a JSON string to XML
+        /// </summary>
+        /// <param name="json">String containing valid JSON</param>
+        /// <param name="deserializeRootElementName"></param>
+        /// <returns>string containing xml</returns>
+        public string JsonToXml(string json, string deserializeRootElementName)
+        {
+            string returnValue;
+
+            // To convert JSON text contained in string json into an XML node
+            if (string.IsNullOrEmpty(deserializeRootElementName))
+            {
+                var doc = JsonConvert.DeserializeXmlNode(json);
+                returnValue = doc.InnerXml;
+            }
+            else
+            {
+                var doc = JsonConvert.DeserializeXmlNode(json, deserializeRootElementName);
+                returnValue = doc.InnerXml;
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Converts an XML string to JSON
+        /// </summary>
+        /// <param name="xml">String containing valid XML</param>
+        /// <param name="eventLog"></param>
+        /// <returns>string containing JSON</returns>
+        public string XmlToJson(string xml)
+        {
+            // To convert an XML node contained in string xml into a JSON string   
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+            return JsonConvert.SerializeXmlNode(doc);
+        }
+    }
+}
---
+++ Json_Prod/Foundation/JsonQuickStart/JsonItemFile.cs
@@ -0,0 +1,40 @@
+﻿/********************************************************************************
+ * JSON QuickStart Library - General Elements used to manipulate JSON data stored on the file system
+ * 
+ * LICENSE: Free to use provided details on fixes and/or extensions emailed to 
+ *      chris.williams@readwatchcreate.com
+********************************************************************************/
+
+namespace JsonQuickStart
+{
+    /// <summary>
+    /// Information about a Json Item stored on the file system
+    /// </summary>
+    public class JsonItemFile
+    {
+        /// <summary>
+        /// This is generated on load in case there are Filenames in other folders with the same name.
+        /// </summary>
+        public string UniqueFileName { get; set;  }
+        
+        /// <summary>
+        /// Type of Json Data being stored (used to determine subfolder beneath JsonRoot to store item
+        /// </summary>
+        public string ItemType { get; set; }
+
+        /// <summary>
+        /// Relative path under Json Root Folder + ItemType
+        /// </summary>
+        public string RelativePathBeneathTable { get; set; }
+
+        /// <summary>
+        /// Name of Json File
+        /// </summary>
+        public string JsonFileName { get; set; }
+
+        /// <summary>
+        /// Json encoded content
+        /// </summary>
+        public string JsonContent { get; set; }
+    }
+}
